c6b86d8212727e5fe3fa41d338c0e0939e755058
Ariel Martinez <ariel.mtz85@gmail.com>
2022-11-29 23:36:15 -0300

    desafio 7

---
+++ desafio7/options/config.js
@@ -0,0 +1,14 @@
+const optionsMysql = {
+  client: "mysql2",
+  connection: {
+    host: "localhost",
+    // port:33060,
+    // user:"coder",
+    // password:"house",
+    user: "root",
+    password: "123456",
+    database: "test",
+  },
+};
+
+
