0c2707862cff1a3241d6a7166185b8959f779767
Ariel Martinez <ariel.mtz85@gmail.com>
2023-03-13 22:06:11 -0300

    desafio 16 corregido

---
+++ desafio16/src/configs/db.config.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import logger from "../utils/loggers/Log4jsLogger.js";
+
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URI, (err) => {
+  err
+    ? logger.error("⛔ Error al conectarse a MongoDB")
+    : logger.info("🆗 Conectados a MongoDB");
+});
+
+export default mongoose;
